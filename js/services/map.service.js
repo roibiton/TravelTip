43d1935788e7fae006a4650671687140c2a80a10
Chen <chenattias19@gmail.com>
2022-08-17 15:31:17 +0300

    update kpi

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -39,7 +39,7 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = '' //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyBvTjJLgXv_JG78L_VC13fO7vJjnzeBzH8'
     var elGoogleApi = document.createElement('script')
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`
     elGoogleApi.async = true
